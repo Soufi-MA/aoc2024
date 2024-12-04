372ae5e8910e128f2a560f544d47194d7c50f233
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-12-04 17:01:54 +0300

    fix: less verbosity

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -4,19 +4,13 @@ import fs from "fs";
 const listPath = path.join(__dirname, "./data/input");
 const input: string[] = fs.readFileSync(listPath, "utf-8").split("\n");
 
-const checkMAS = (x: number, y: number) => {
-  if (
-    (input[x + 1][y + 1] === "M" && input[x - 1][y - 1] === "S") ||
-    (input[x - 1][y - 1] === "M" && input[x + 1][y + 1] === "S")
-  ) {
-    if (
-      (input[x - 1][y + 1] === "M" && input[x + 1][y - 1] === "S") ||
-      (input[x + 1][y - 1] === "M" && input[x - 1][y + 1] === "S")
-    ) {
-      return true;
-    }
-  }
-  return false;
+const checkMAS = (x: number, y: number): boolean => {
+  const match1 = input[x + 1][y + 1] === "M" && input[x - 1][y - 1] === "S";
+  const match2 = input[x + 1][y + 1] === "S" && input[x - 1][y - 1] === "M";
+  const match3 = input[x - 1][y + 1] === "M" && input[x + 1][y - 1] === "S";
+  const match4 = input[x - 1][y + 1] === "S" && input[x + 1][y - 1] === "M";
+
+  return (match1 || match2) && (match3 || match4);
 };
 
 let sum = 0;
@@ -24,15 +18,13 @@ for (let x = 0; x < input.length; x++) {
   for (let y = 0; y < input[x].length; y++) {
     if (
       input[x][y] === "A" &&
-      input.length - x > 1 &&
-      input[x].length - y > 1 &&
-      x !== 0 &&
-      y !== 0
+      x > 0 &&
+      y > 0 &&
+      x < input.length - 1 &&
+      y < input[x].length - 1 &&
+      checkMAS(x, y)
     ) {
-      const match = checkMAS(x, y);
-      if (match) {
-        sum++;
-      }
+      sum++;
     }
   }
 }
